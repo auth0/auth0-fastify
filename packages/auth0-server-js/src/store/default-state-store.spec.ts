bc801c83b0092f1ec300208997ff90ca14240c8c
Frederik Prijck <frederik.prijck@okta.com>
2025-02-26 00:52:01 +0100

    chore(auth0-server-js): Add tests for default stores

---
+++ packages/auth0-server-js/src/store/default-state-store.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from 'vitest';
+import type { StateData } from '../types.js';
+import { DefaultStateStore } from './default-state-store.js';
+
+test('should get, set and delete', async () => {
+  const store = new DefaultStateStore({ secret: '<secret>' });
+
+  const stateData: StateData = {
+    user: { sub: '<sub>' },
+    id_token: '<id_token>',
+    refresh_token: '<refresh_token>',
+    tokenSets: [],
+    internal: { sid: '<sid>', createdAt: 0 },
+  };
+
+  await store.set('<identifier>', stateData);
+
+  const cachedData = await store.get('<identifier>');
+  expect(cachedData).toBeDefined();
+  expect(cachedData?.id_token).toBe('<id_token>');
+  expect(cachedData?.refresh_token).toBe('<refresh_token>');
+  expect(cachedData?.internal.sid).toBe('<sid>');
+  expect(cachedData?.user!.sub).toBe('<sub>');
+
+  await store.delete('<identifier>');
+  await expect(store.get('<identifier>')).resolves.toBeUndefined();
+});
