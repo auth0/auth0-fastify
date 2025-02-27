7a8745287c40ef85aa622ef649bb0e2ccbc9010d
Frederik Prijck <frederik.prijck@okta.com>
2025-02-27 21:17:55 +0100

    feat(auth0-server-js): Remove public init method and call lazily internally

--- packages/auth0-server-js/src/errors/index.ts
+++ packages/auth0-server-js/src/errors/index.ts
@@ -1,13 +1,3 @@
-export class ClientNotInitializedError extends Error {
-  public code: string = 'client_not_initialized_error';
-
-  constructor(message?: string) {
-    super(message ?? 'The client was not initialized. Ensure to call `init()`.');
-
-    this.name = 'ClientNotInitializedError';
-  }
-}
-
 export class MissingStateError extends Error {
   public code: string = 'missing_state_error';
 
