52d0ffe41d16c30b178d013dd927678f8f36677d
Frederik Prijck <frederik.prijck@okta.com>
2025-03-03 11:05:48 +0100

    feat(auth0-auth-js): Ensure stripUndefinedProperties only strips undefined

--- packages/auth0-auth-js/src/utils.ts
+++ packages/auth0-auth-js/src/utils.ts
@@ -6,6 +6,6 @@ export function stripUndefinedProperties<T extends object>(
   value: T
 ): Partial<T> {
   return Object.entries(value)
-    .filter(([, value]) => !!value)
+    .filter(([, value]) => typeof value !== 'undefined')
     .reduce((acc, curr) => ({ ...acc, [curr[0]]: curr[1] }), {});
 }
