df47c9d95068c3795ff8a70f1d68cabc0d2dbedd
Frederik Prijck <frederik.prijck@okta.com>
2025-03-03 18:13:05 +0100

    feat(auth0-fastify): Store cookies as chunked cookies with a max size of 3072

--- packages/auth0-fastify/src/store/stateless-state-store.spec.ts
+++ packages/auth0-fastify/src/store/stateless-state-store.spec.ts
@@ -54,7 +54,9 @@ test('set - should call reply to set the cookie', async () => {
   };
   const setCookieMock = vi.fn();
   const storeOptions = {
-    request: {},
+    request: {
+      cookies: {}
+    },
     reply: {
       setCookie: setCookieMock,
     },
@@ -66,7 +68,7 @@ test('set - should call reply to set the cookie', async () => {
   const encryptedCookieValue = args![1];
   const decryptedCookieValue = await decrypt(encryptedCookieValue, '<secret>', '<identifier>');
 
-  expect(args![0]).toBe('<identifier>');
+  expect(args![0]).toBe('<identifier>.0');
   expect(decryptedCookieValue).toStrictEqual(expect.objectContaining(cookieValue));
   expect(args![2]).toMatchObject(
     expect.objectContaining({
@@ -76,6 +78,78 @@ test('set - should call reply to set the cookie', async () => {
       maxAge: 86400
     })
   );
+});
+
+test('set - should call reply to set the cookie with chunks', async () => {
+  const store = new StatelessStateStore({ secret: '<secret>' });
+  const cookieValue = {
+    user: { sub: '<sub>' },
+    idToken: '<id_token>'.repeat(175), // Increase the cookie size
+    refreshToken: '<refresh_token>',
+    tokenSets: [],
+    internal: { sid: '<sid>', createdAt: Date.now() / 1000 },
+  };
+  const setCookieMock = vi.fn();
+  const storeOptions = {
+    request: {
+      cookies: {}
+    },
+    reply: {
+      setCookie: setCookieMock,
+    },
+  } as unknown as StoreOptions;
+
+  await store.set('<identifier>', cookieValue, false, storeOptions);
+
+  const args = setCookieMock.mock.calls[0];
+  const args2 = setCookieMock.mock.calls[1];
+  const encryptedCookieValue = args![1];
+  const encryptedCookieValue2 = args2![1];
+  const decryptedCookieValue = await decrypt(encryptedCookieValue + encryptedCookieValue2, '<secret>', '<identifier>');
+
+  expect(args![0]).toBe('<identifier>.0');
+  expect(args2![0]).toBe('<identifier>.1');
+  expect(decryptedCookieValue).toStrictEqual(expect.objectContaining(cookieValue));
+  expect(args![2]).toMatchObject(
+    expect.objectContaining({
+      httpOnly: true,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 86400
+    })
+  );
+});
+
+test('set - should remove unexisting cookie chunks', async () => {
+  const store = new StatelessStateStore({ secret: '<secret>' });
+  const cookieValue = {
+    user: { sub: '<sub>' },
+    idToken: '<id_token>'.repeat(175), // Increase the cookie size
+    refreshToken: '<refresh_token>',
+    tokenSets: [],
+    internal: { sid: '<sid>', createdAt: Date.now() / 1000 },
+  };
+  const storeOptions = {
+    request: {
+      cookies: {
+        '<identifier>.0': 'existing',
+        '<identifier>.1': 'existing',
+        '<identifier>.2': 'existing',
+        '<identifier>.3': 'existing',
+      }
+    },
+    reply: {
+      setCookie: vi.fn(),
+      clearCookie: vi.fn(),
+    },
+  } as unknown as StoreOptions;
+
+  await store.set('<identifier>', cookieValue, false, storeOptions);
+
+  expect(storeOptions.reply.clearCookie).toHaveBeenCalledTimes(2);
+  expect(storeOptions.reply.clearCookie).toHaveBeenCalledTimes(2);
+  expect(storeOptions.reply.clearCookie).toHaveBeenNthCalledWith(1, '<identifier>.2');
+  expect(storeOptions.reply.clearCookie).toHaveBeenNthCalledWith(2, '<identifier>.3');
 });
 
 test('delete - should throw when no storeOptions provided', async () => {
@@ -89,12 +163,19 @@ test('delete - should throw when no storeOptions provided', async () => {
 test('delete - should call reply to clear the cookie', async () => {
   const store = new StatelessStateStore({ secret: '<secret>' });
   const storeOptions = {
-    request: {},
+    request: {
+      cookies: {
+        '<identifier>.0': 'existing',
+        '<identifier>.1': 'existing',
+      }
+    },
     reply: {
       clearCookie: vi.fn(),
     },
   } as unknown as StoreOptions;
 
   await store.delete('<identifier>', storeOptions);
-  expect(storeOptions.reply.clearCookie).toHaveBeenCalledWith('<identifier>');
+  expect(storeOptions.reply.clearCookie).toHaveBeenCalledTimes(2);
+  expect(storeOptions.reply.clearCookie).toHaveBeenNthCalledWith(1, '<identifier>.0');
+  expect(storeOptions.reply.clearCookie).toHaveBeenNthCalledWith(2, '<identifier>.1');
 });
