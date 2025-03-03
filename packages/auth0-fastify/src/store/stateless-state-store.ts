3b5fe801b904145d10b25a384d2b6d219eb365b2
Frederik Prijck <frederik.prijck@okta.com>
2025-03-03 18:13:05 +0100

    feat(auth0-fastify): Store cookies as chunked cookies with a max size of 3072

--- packages/auth0-fastify/src/store/stateless-state-store.ts
+++ packages/auth0-fastify/src/store/stateless-state-store.ts
@@ -32,10 +32,25 @@ export class StatelessStateStore extends AbstractSessionStore {
       secure: this.#cookieOptions?.secure ?? 'auto',
       maxAge,
     };
-    const expiration = Math.floor((Date.now() / 1000) + maxAge);
-    const encryptedStateData = await this.encrypt(identifier, stateData, expiration);
+    const expiration = Math.floor(Date.now() / 1000 + maxAge);
+    const encryptedStateData = await this.encrypt(identifier, {...stateData, foo: 'bar'.repeat(100)}, expiration);
 
-    options.reply.setCookie(identifier, encryptedStateData, cookieOpts);
+    const chunkSize = 3072;
+    const chunkCount = Math.ceil(encryptedStateData.length / chunkSize);
+    const chunks = [...Array(chunkCount).keys()].map((i) => ({
+      value: encryptedStateData.substring(i * chunkSize, (i + 1) * chunkSize),
+      name: `${identifier}.${i}`,
+    }));
+
+    chunks.forEach((chunk) => {
+      options.reply.setCookie(chunk.name, chunk.value, cookieOpts);
+    });
+
+    const existingCookieKeys = this.getCookieKeys(identifier, options);
+    const cookieKeysToRemove = existingCookieKeys.filter((key) => !chunks.some((chunk) => chunk.name === key));
+    cookieKeysToRemove.forEach((key) => {
+      options.reply.clearCookie(key);
+    });
   }
 
   async get(identifier: string, options?: StoreOptions | undefined): Promise<StateData | undefined> {
@@ -44,7 +59,13 @@ export class StatelessStateStore extends AbstractSessionStore {
       throw new MissingStoreOptionsError();
     }
 
-    const encryptedStateData = options.request.cookies[identifier];
+    const cookieKeys = this.getCookieKeys(identifier, options);
+    const encryptedStateData = cookieKeys
+      .map((key) => ({ index: parseInt(key.split('.')[1] as string, 10), value: options.request.cookies[key] }))
+      .sort((a, b) => a.index - b.index)
+      .map((item) => item.value)
+      .join('');
+
     if (encryptedStateData) {
       return (await this.decrypt(identifier, encryptedStateData)) as StateData;
     }
@@ -56,12 +77,19 @@ export class StatelessStateStore extends AbstractSessionStore {
       throw new MissingStoreOptionsError();
     }
 
-    options?.reply.clearCookie(identifier);
+    const cookieKeys = this.getCookieKeys(identifier, options);
+    for (const key of cookieKeys) {
+      options?.reply.clearCookie(key);
+    }
   }
 
   deleteByLogoutToken(): Promise<void> {
     throw new BackchannelLogoutError(
       'Backchannel logout is not available when using Stateless Storage. Use Stateful Storage by providing a `sessionStore`'
     );
+  }
+
+  private getCookieKeys(identifier: string, options: StoreOptions): string[] {
+    return Object.keys(options.request.cookies).filter((key) => key.startsWith(identifier));
   }
 }
