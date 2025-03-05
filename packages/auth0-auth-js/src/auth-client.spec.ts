b1273ec422548c3a87f22f64d50eee7173f1fe01
Frederik Prijck <frederik.prijck@okta.com>
2025-03-05 18:44:07 +0100

    chore(auth0-auth-js): Add additional tests

--- packages/auth0-auth-js/src/auth-client.spec.ts
+++ packages/auth0-auth-js/src/auth-client.spec.ts
@@ -1,4 +1,12 @@
-import { expect, test, afterAll, beforeAll, beforeEach, vi } from 'vitest';
+import {
+  expect,
+  test,
+  afterAll,
+  beforeAll,
+  beforeEach,
+  vi,
+  afterEach,
+} from 'vitest';
 import { setupServer } from 'msw/node';
 import { http, HttpResponse } from 'msw';
 import { AuthClient } from './auth-client.js';
@@ -7,7 +15,8 @@ import { generateToken } from './test-utils/tokens.js';
 
 const domain = 'auth0.local';
 let accessToken: string;
-const mockOpenIdConfiguration = {
+let accessTokenWithAudienceAndBindingMessage: string;
+let mockOpenIdConfiguration = {
   issuer: `https://${domain}/`,
   authorization_endpoint: `https://${domain}/authorize`,
   backchannel_authentication_endpoint: `https://${domain}/custom-authorize`,
@@ -22,35 +31,78 @@ const restHandlers = [
   }),
   http.post(
     mockOpenIdConfiguration.backchannel_authentication_endpoint,
-    async () => {
-      const auth_req_id = 'auth_req_123';
+    async ({ request }) => {
+      const info = await request.formData();
+      const shouldFailBCAuthorize = !!info.get('should_fail_authorize');
 
-      return HttpResponse.json({
-        auth_req_id: auth_req_id,
-        interval: 0.5,
-        expires_in: 60,
-      });
+      let auth_req_id = 'auth_req_123';
+
+      if (info.get('audience') && info.get('binding_message')) {
+        auth_req_id = 'auth_req_789';
+      }
+
+      if (info.get('should_fail_token_exchange')) {
+        auth_req_id = 'auth_req_should_fail';
+      }
+
+      if (info.get('authorization_details')) {
+        auth_req_id = 'auth_req_with_authorization_details';
+      }
+
+      return shouldFailBCAuthorize
+        ? HttpResponse.json(
+            { error: '<error_code>', error_description: '<error_description>' },
+            { status: 400 }
+          )
+        : HttpResponse.json({
+            auth_req_id: auth_req_id,
+            interval: 0.5,
+            expires_in: 60,
+          });
     }
   ),
-  http.post(mockOpenIdConfiguration.token_endpoint, async () => {
-    return HttpResponse.json({
-      access_token: accessToken,
-      id_token: await generateToken(domain, 'user_123', '<client_id>'),
-      expires_in: 60,
-      token_type: 'Bearer',
-      scope: '<scope>',
-    });
+  http.post(mockOpenIdConfiguration.token_endpoint, async ({ request }) => {
+    const info = await request.formData();
+    let accessTokenToUse = accessToken;
+    if (info.get('auth_req_id') === 'auth_req_789') {
+      accessTokenToUse = accessTokenWithAudienceAndBindingMessage;
+    }
+    const shouldFailTokenExchange =
+      info.get('auth_req_id') === 'auth_req_should_fail';
+
+    return shouldFailTokenExchange
+      ? HttpResponse.json(
+          { error: '<error_code>', error_description: '<error_description>' },
+          { status: 400 }
+        )
+      : HttpResponse.json({
+          access_token: accessTokenToUse,
+          id_token: await generateToken(domain, 'user_123', '<client_id>'),
+          expires_in: 60,
+          token_type: 'Bearer',
+          scope: '<scope>',
+          ...(info.get('auth_req_id') === 'auth_req_with_authorization_details'
+            ? { authorization_details: [{ type: 'accepted' }] }
+            : {}),
+        });
   }),
+
   http.post(
     mockOpenIdConfiguration.pushed_authorization_request_endpoint,
-    () => {
-      return HttpResponse.json(
-        {
-          request_uri: 'request_uri_123',
-          expires_in: 60,
-        },
-        { status: 201 }
-      );
+    async ({ request }) => {
+      const info = await request.formData();
+      return info.get('fail')
+        ? HttpResponse.json(
+            { error: '<error_code>', error_description: '<error_description>' },
+            { status: 400 }
+          )
+        : HttpResponse.json(
+            {
+              request_uri: 'request_uri_123',
+              expires_in: 60,
+            },
+            { status: 201 }
+          );
     }
   ),
 ];
@@ -65,6 +117,22 @@ afterAll(() => server.close());
 
 beforeEach(async () => {
   accessToken = await generateToken(domain, 'user_123');
+  accessTokenWithAudienceAndBindingMessage = await generateToken(
+    domain,
+    'user_789'
+  );
+});
+
+afterEach(() => {
+  mockOpenIdConfiguration = {
+    issuer: `https://${domain}/`,
+    authorization_endpoint: `https://${domain}/authorize`,
+    backchannel_authentication_endpoint: `https://${domain}/custom-authorize`,
+    token_endpoint: `https://${domain}/custom/token`,
+    end_session_endpoint: `https://${domain}/logout`,
+    pushed_authorization_request_endpoint: `https://${domain}/pushed-authorize`,
+  };
+  server.resetHandlers();
 });
 
 test('configuration - should use customFetch', async () => {
@@ -91,4 +159,175 @@ test('configuration - should use customFetch', async () => {
 
   expect(tokenResponse.accessToken).toBe(accessToken);
   expect(mockFetch).toHaveBeenCalledTimes(1);
+});
+
+test('buildAuthorizationUrl - should throw when using PAR without PAR support', async () => {
+  const serverClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      redirect_uri: '/test_redirect_uri',
+    },
+  });
+
+  // @ts-expect-error Ignore the fact that this property is not defined as optional in the test.
+  delete mockOpenIdConfiguration.pushed_authorization_request_endpoint;
+
+  await expect(
+    serverClient.buildAuthorizationUrl({ pushedAuthorizationRequests: true })
+  ).rejects.toThrowError(
+    'The Auth0 tenant does not have pushed authorization requests enabled. Learn how to enable it here: https://auth0.com/docs/get-started/applications/configure-par'
+  );
+});
+
+test('buildAuthorizationUrl - should build the authorization url for PAR', async () => {
+  const serverClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      redirect_uri: '/test_redirect_uri',
+    },
+  });
+
+  const { authorizationUrl } = await serverClient.buildAuthorizationUrl({
+    pushedAuthorizationRequests: true,
+  });
+
+  expect(authorizationUrl.host).toBe(domain);
+  expect(authorizationUrl.pathname).toBe('/authorize');
+  expect(authorizationUrl.searchParams.get('client_id')).toBe('<client_id>');
+  expect(authorizationUrl.searchParams.get('request_uri')).toBe(
+    'request_uri_123'
+  );
+  expect(authorizationUrl.searchParams.size).toBe(2);
+});
+
+test('buildAuthorizationUrl - should throw when building the authorization url for PAR failed', async () => {
+  const serverClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      redirect_uri: '/test_redirect_uri',
+      fail: true,
+    },
+  });
+
+  await expect(
+    serverClient.buildAuthorizationUrl({
+      pushedAuthorizationRequests: true,
+    })
+  ).rejects.toThrowError(
+    expect.objectContaining({
+      code: 'build_authorization_url_error',
+      message:
+        'There was an error when trying to build the authorization URL. Check the server logs for more information.',
+      cause: expect.objectContaining({
+        error: '<error_code>',
+        error_description: '<error_description>',
+      }),
+    })
+  );
+});
+
+test('backchannelAuthentication - should return the access token from the token endpoint when passing audience and binding_message', async () => {
+  const authClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      audience: '<audience>',
+    },
+  });
+
+  const response = await authClient.backchannelAuthentication({
+    bindingMessage: '<binding_message>',
+    loginHint: { sub: '<sub>' },
+  });
+
+  expect(response.accessToken).toBe(accessTokenWithAudienceAndBindingMessage);
+});
+
+test('backchannelAuthentication - should support RAR', async () => {
+  const authClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      audience: '<audience>',
+    },
+  });
+
+  const response = await authClient.backchannelAuthentication({
+    bindingMessage: '<binding_message>',
+    loginHint: { sub: '<sub>' },
+    authorizationParams: {
+      authorization_details: JSON.stringify([
+        {
+          type: 'accepted',
+        },
+      ]),
+    },
+  });
+
+  // When we send authorization_details, we should get it back in the response
+  expect(response.authorizationDetails?.[0]!.type).toBe('accepted');
+});
+
+test('backchannelAuthentication - should throw an error when bc-authorize failed', async () => {
+  const authClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      should_fail_authorize: true,
+    },
+  });
+
+  await expect(
+    authClient.backchannelAuthentication({
+      loginHint: { sub: '<sub>' },
+      bindingMessage: '<binding_message>',
+    })
+  ).rejects.toThrowError(
+    expect.objectContaining({
+      code: 'login_backchannel_error',
+      message:
+        'There was an error when trying to use Client-Initiated Backchannel Authentication. Check the server logs for more information.',
+      cause: expect.objectContaining({
+        error: '<error_code>',
+        error_description: '<error_description>',
+      }),
+    })
+  );
+});
+
+test('backchannelAuthentication - should throw an error when token exchange failed', async () => {
+  const authClient = new AuthClient({
+    domain,
+    clientId: '<client_id>',
+    clientSecret: '<client_secret>',
+    authorizationParams: {
+      should_fail_token_exchange: true,
+    },
+  });
+
+  await expect(
+    authClient.backchannelAuthentication({
+      loginHint: { sub: '<sub>' },
+      bindingMessage: '<binding_message>',
+    })
+  ).rejects.toThrowError(
+    expect.objectContaining({
+      code: 'login_backchannel_error',
+      message:
+        'There was an error when trying to use Client-Initiated Backchannel Authentication. Check the server logs for more information.',
+      cause: expect.objectContaining({
+        error: '<error_code>',
+        error_description: '<error_description>',
+      }),
+    })
+  );
 });
