62992d6ca6c1b19a0d83e84855fd6ddf1e0f42d4
Frederik Prijck <frederik.prijck@okta.com>
2025-03-07 17:32:37 +0100

    chore: Add account linking to fastify sample

--- examples/example-fastify/src/index.ts
+++ examples/example-fastify/src/index.ts
@@ -68,6 +68,38 @@ fastify.get(
   }
 );
 
+fastify.get(
+  '/connect/google',
+  {
+    preHandler: hasSessionPreHandler,
+  },
+  async (request, reply) => {
+    const callbackPath = '/connect/callback';
+    const redirectUri = new URL(callbackPath, appBaseUrl);
+    const linkUserUrl = await fastify.auth0Client!.startLinkUser({ 
+      connection: 'google-oauth2',
+      connectionScope: 'https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events.readonly',
+      authorizationParams: {
+        redirect_uri: redirectUri.toString(),
+      }
+    }, { request, reply });
+
+    reply.redirect(linkUserUrl.href);
+  }
+);
+
+fastify.get(
+  '/connect/callback',
+  {
+    preHandler: hasSessionPreHandler,
+  },
+  async (request, reply) => {
+    await fastify.auth0Client!.completeInteractiveLogin(new URL(request.url, appBaseUrl), { request, reply });
+
+    reply.redirect(appBaseUrl);
+  }
+);
+
 const start = async () => {
   try {
     await fastify.listen({ port: 3000 });
